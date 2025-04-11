94cf33efee5f845b219e47e39df398c173d400b2
lukauu <156195421+lukauu@users.noreply.github.com>
2025-04-11 18:37:56 +0300

    Seats are now stored in a nested list to create a grid of seats instead of a list

--- moviehall.py
+++ moviehall.py
@@ -2,9 +2,11 @@ from seat import Seat
 class MovieHall:
     __id_counter = 1
 
-    def __init__(self, type, seat_count):
+    def __init__(self, type, rows: int, collumns: int):
         self.__type = type # Type will be child class
-        self.__seats = [Seat() for _ in range(seat_count)]
+        self.__rows = rows
+        self.__collumns = collumns
+        self.__seats = [[Seat(row, collum) for collum in range(self.__collumns)] for row in range(self.__rows)]
         MovieHall.__id_counter += 1
     
     @property
@@ -19,11 +21,12 @@ class MovieHall:
     def seats(self):
         return self.__seats
     
-    def reserve_seat(self, seat_number):
-        self.__seats[seat_number].reserve_seat()
+    def reserve_seat(self, row, collumn):
+        self.__seats[row][collumn].reserve_seat()
 
     def display_seats(self):
-        print([str(seat) for seat in self.__seats])
+        for row in self.__seats:
+            print(" ".join(str(seat) for seat in row))
 
     def __str__(self):
         return f"Moviehall ID: {self.__id_counter}, Moviehall type: {self.__type}"
