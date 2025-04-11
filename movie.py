e4456399ac9c9bc4754732b9a534d7bfe1c6840b
Lauri Korpelainen <lauri.korpelainen@edu.turkuamk.fi>
2025-04-11 12:14:44 +0300

    fixed merge conflicts

--- movie.py
+++ movie.py
@@ -1,21 +1,13 @@
 class Movie:
-<<<<<<< HEAD
-    def __init__(self, id: str, title: str, length, director: str, age_rating: int):
-        self.__id = id
-        self.__title = title
-        self.__length = length
-        self.__director = director
-        self.__age_rating = age_rating
-=======
     __id_counter = 1
 
-    def __init__(self, title: str, length, director: str, genre: str):
+    def __init__(self, title: str, length, director: str, genre: str, age_rating: int):
         self.__title = title
         self.__length = length
         self.__director = director
         self.__genre = genre
+        self.__age_rating = age_rating
         Movie.__id_counter += 1
->>>>>>> lauri
 
     @property
     def id(self):
@@ -33,11 +25,9 @@ class Movie:
     def director(self):
         return self.__director
     
-<<<<<<< HEAD
     @property
     def age_rating(self):
         return self.__age_rating
-=======
+
     def __str__(self):
         return f"Movie ID: {self.__id_counter}, Title: {self.__title}, Director: {self.__director}, Genre: {self.__genre}, Length: {self.__length}"
->>>>>>> lauri
--- moviehall.py
+++ moviehall.py
@@ -1,15 +1,9 @@
 from seat import Seat
 class MovieHall:
-<<<<<<< HEAD
-    def __init__(self, id: str, name: str, seat_count: int):
-        self.__id = id
-        self.__name = name
-=======
     __id_counter = 1
 
     def __init__(self, type, seat_count):
         self.__type = type # Type will be child class
->>>>>>> lauri
         self.__seats = [Seat() for _ in range(seat_count)]
         MovieHall.__id_counter += 1
     
@@ -22,20 +16,14 @@ class MovieHall:
         return self.__id
     
     @property
-    def name(self):
-        return self.__name
-    
-    @property
     def seats(self):
         return self.__seats
     
-<<<<<<< HEAD
     def reserve_seat(self, seat_number):
         self.__seats[seat_number].reserve_seat()
 
     def display_seats(self):
         print([str(seat) for seat in self.__seats])
-=======
+        
     def __str__(self):
         return f"Moviehall ID: {self.__id_counter}, Moviehall type: {self.__type}"
->>>>>>> lauri
