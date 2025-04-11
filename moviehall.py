9c03c02678b6d1762cb23d273c5108f876dc050e
lukauu <156195421+lukauu@users.noreply.github.com>
2025-04-11 11:17:46 +0300

    Added gitignore & seat adding logic to movie hall

--- moviehall.py
+++ moviehall.py
@@ -1,3 +1,10 @@
+from seat import Seat
 class MovieHall:
-    def __init__(self):
-        pass
+    def __init__(self, id, name, seat_count):
+        self.__id = id
+        self.__name = name
+        self.__seats = [Seat() for _ in range(seat_count)]
+
+    @property
+    def seats(self):
+        return self.__seats
