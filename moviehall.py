d28a8671b064c697cfccfcb3790102106c360e81
lukauu <156195421+lukauu@users.noreply.github.com>
2025-04-11 11:21:15 +0300

    added getters to movie hall

--- moviehall.py
+++ moviehall.py
@@ -1,10 +1,18 @@
 from seat import Seat
 class MovieHall:
-    def __init__(self, id, name, seat_count):
+    def __init__(self, id: str, name: str, seat_count: int):
         self.__id = id
         self.__name = name
         self.__seats = [Seat() for _ in range(seat_count)]
 
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def name(self):
+        return self.__name
+    
     @property
     def seats(self):
         return self.__seats
